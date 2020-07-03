d137811fa6ec8082dc42ad55c28420553611c68a
Songyu-Wang <wangsongyuf@gmail.com>
2020-07-02 22:06:32 -0700

    Well since no plan to publish this, it does not need to be polished too much

--- zContent.js
+++ zContent.js
@@ -1,22 +1,24 @@
 // Add a z to the file name because I am too lazy to figure out how to load jquery before this script
 
 // From https://gist.github.com/chrisjhoughton/7890303
-var waitForEl = function(selector, callback) {
+var waitForEl = function (selector, callback) {
   if ($(selector).length) {
     callback();
   } else {
-    setTimeout(function() {
+    setTimeout(function () {
       waitForEl(selector, callback);
     }, 100);
   }
 };
-const mainSelector = 'a[href="/apps/github-actions"]'
-waitForEl(mainSelector, function() {
-    document.querySelectorAll(mainSelector).forEach(function(item) {
-    item.closest('.push').style.display='none';
-})
-});
+const mainSelector = 'a[href="/apps/github-actions"]';
+function run() {
+  waitForEl(mainSelector, function () {
+    document.querySelectorAll(mainSelector).forEach(function (item) {
+      item.closest(".push").style.display = "none";
+    });
+  });
+}
 
-
-
-
+window.setInterval(function () {
+  run();
+}, 1000);
